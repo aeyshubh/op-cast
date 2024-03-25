fd73dd464e23608caf1f33e64660d289698e71dd
Shubham Patel <shubhamashishpatel@gmail.com>
2024-03-26 01:19:13 +0530

    added vercel url

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
 // use NODE_ENV to not have to change config based on where it's deployed
-export const NEXT_PUBLIC_URL ='https://d2bd-150-129-206-132.ngrok-free.app';
-  process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://zizzamia.xyz';
+export const NEXT_PUBLIC_URL ='https://op-cast.vercel.app/';
+  process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://op-cast.vercel.app/';
 export const TOKEN_ADDRESS = '0xe0526c748C85b7a277E34FEfbfC94e9687140bD4';
